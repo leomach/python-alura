e91ea277fd5707fe690793e7b227d45778266c99
Leandro Machado <leandrovictoram@gmail.com>
2024-05-24 14:59:02 -0300

    first commit

---
+++ oo-sabor-express/modelos/restaurante.py
@@ -0,0 +1,12 @@
+class Restaurante:
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
