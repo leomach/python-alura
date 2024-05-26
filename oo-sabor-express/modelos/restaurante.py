4a09c60148c5ba958eaad04ab9827177dc5af314
Leandro Machado <leandrovictoram@gmail.com>
2024-05-25 21:00:32 -0300

    OO: completo

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,21 +1,46 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}\n')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacao).ljust(25)} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Desativado'
+    
+    def alternar_ativo(self):
+        self._ativo = not self._ativo
+        print(f'O restaurante {self._nome} foi {self.ativo}.')
 
-Restaurante.listar_restaurantes()
+    def receber_avaliacao(self, cliente, nota):
+        if 0 <= nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+        else:
+            print('Nota inválida.')
 
+    @property
+    def media_avaliacao(self):
+        if len(self._avaliacao) > 0:
+            soma = 0
+            for avaliacao in self._avaliacao:
+                soma += avaliacao._nota
+            media = soma / len(self._avaliacao)
+            return round(media, 1)
+        else:
+            return '-'
